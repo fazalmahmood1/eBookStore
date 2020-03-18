0becd025d9da28b5da749b1ba82867fe3a3bd0f4
Fazal Mahmood <fazalpk786@gmail.com>
2020-03-18 15:46:34 +0500

    implemented templating engine express-handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,13 @@ const products = [];
 // admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   // res.sendFile(path.join(rootDir,'views', 'add-product.html'));
-  res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
+  res.render('add-product', {
+    pageTitle: 'Add Product', 
+    path: '/admin/add-product',
+    formsCss: true,
+    productCss: true,
+    activeAddProduct: true
+  });
 });
 
 // admin/add-product => POST
