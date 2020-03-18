cfc9285f37a712f457be6e6f03a063cf822a6c1c
Fazal Mahmood <fazalpk786@gmail.com>
2020-03-18 13:11:50 +0500

    Added dynamic content and implemented pug templating engine

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,16 @@
+const path = require('path');
 const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
+  // console.log(adminData.products);
+  // res.sendFile(path.join(rootDir,'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {prods: products, pageTitle: 'Shop', path:'/'});
 });
 
 module.exports = router;
