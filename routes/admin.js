b2d234b514277a9d739d6e5c46d15f33a563b16a
Fazal Mahmood <fazalpk786@gmail.com>
2020-03-18 18:02:37 +0500

    added MVC and enhance project structure

--- routes/admin.js
+++ routes/admin.js
@@ -1,29 +1,18 @@
 const path = require('path');
+
 const express = require('express');
 
-const rootDir = require('../util/path');
+const adminController = require('../controllers/admin');
+
 const router = express.Router();
 
-const products = [];
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-// admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  // res.sendFile(path.join(rootDir,'views', 'add-product.html'));
-  res.render('add-product', {
-    pageTitle: 'Add Product', 
-    path: '/admin/add-product',
-    formsCss: true,
-    productCss: true,
-    activeAddProduct: true
-  });
-});
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
-// admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  // console.log(req.body);
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
-exports.routes = router;
-exports.products = products; 
+module.exports = router;
